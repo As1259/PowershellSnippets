ae6c3a568eea779f6025c6fc7856671cd90371c3
Andreas Schreiner <schreiner.andreas@outlook.com>
2019-02-26 21:46:02 +0100

    doc added

--- getProcessUsernames.ps1
+++ getProcessUsernames.ps1
@@ -1,4 +1,5 @@
-﻿Param(
+﻿## <copyright>Andreas Schreiner 2019</copyright>
+Param(
   [string]$ProcessName
 )
 (Get-WmiObject -Class Win32_Process | Where-Object {$_.ProcessName -eq $ProcessName}).GetOwner() | Format-Table Domain, User 
